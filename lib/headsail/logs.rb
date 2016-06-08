9ab3863c33a6db611e54e1a25555a5b0418104c0
Nicholas Sweeting <nsweeting@gmail.com>
2016-06-08 19:46:18 -0400

    Updated logger

--- lib/headsail/logs.rb
+++ lib/headsail/logs.rb
@@ -2,7 +2,7 @@ module Headsail
   STDOUT.sync = true
 
   def self.log
-    @logger ||= Logger.new(STDOUT)
+    @logger ||= build_logger
   end
 
   def self.info(msg)
@@ -11,5 +11,13 @@ module Headsail
 
   def self.err(msg)
     log.info("[ \e[31m#{'ERR'}\e[0m ] #{msg}")
+  end
+
+  def self.build_logger
+    logger = Logger.new(STDOUT)
+    logger.formatter = proc do |severity, datetime, progname, msg|
+      "#{severity}: #{msg}\n"
+    end
+    logger
   end
 end
